11ac704cb74ca8ba8858777c42f68927829143e9
LiWenjin <595082809@qq.com>
2022-01-12 11:32:01 +0800

    添加git操作说明

---
+++ CFileOpr/mmapOpt.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <unist.h>
+
+int main(void) {
+    
+    return 0;
+}
---
+++ git/GitOpt.md
@@ -0,0 +1,12 @@
+# git操作
+### 1、mac下添加公钥
+```bash
+ssh-keygen -t rsa -C "595082809@qq.com"
+cd ~/.ssh
+cat id_rsa.pub 
+```
+### 2、添加文档
+```bash
+
+```
+
